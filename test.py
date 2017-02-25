74e0463a8c34e2a0b40dde54e44e31a70ef89aaf
Daniele Strafile <gerrystrafi@hotmail.it>
2017-02-25 10:42:06 +0000

    created first test to read a file from web

--- test.py
+++ test.py
@@ -3,18 +3,28 @@ Created on 20 Feb 2017
 
 @author: Daniele
 
+"""
+
 import sys
 
-from nose.tools import ok_,eq_
+from nose.tools import *
+from src.led_tester import *
 
 
-from src.main import calculate
-
+uri = "http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3.txt"
 
 def test_calculate():
-    ok_(calculate(2,3) == 6, 'calculation is incorrect')
-    
+    eq_(len(read_uri(uri)), 9506, "buffer length does not match" )
+            
+"""    
 def test_version():
     eq_(sys.version_info[0],3,"Python is not version 3")
-    
-"""
+"""   
+
+
+
+
+
+
+
+
