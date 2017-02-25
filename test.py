68015d801c2424b3503beaf31cc9163e4aa80951
Daniele Strafile <gerrystrafi@hotmail.it>
2017-02-25 11:53:11 +0000

    created test_inputline2() and 3 to test invalid input, passed!

--- test.py
+++ test.py
@@ -9,6 +9,7 @@ import sys
 
 from nose.tools import *
 from src.led_tester import *
+from _overlapped import NULL
 
 
 uri = "http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3.txt"
@@ -16,11 +17,23 @@ uri = "http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3.txt"
 def test_calculate():
     eq_(len(read_uri(uri)), 9506, "buffer length does not match" )
             
-def test_inputline(line, expected):
+def test_inputline():
     line = "turn on 0,0 through 11,11"
     expected = ["turn on",0,0,11,11]
     result = extract(line)
     eq_(result, expected, "[]".format(result))
+    
+def test_inputline2():
+    line = "turn on 0, through 11,11"
+    expected = None
+    result = extract(line)
+    eq_(result, expected, "The function should have returned none")
+
+def test_inputline3():
+    line = "churn on 0,0 through 11,11"
+    expected = None
+    result = extract(line)
+    eq_(result, expected, "The function should have returned none")
 
 
 
