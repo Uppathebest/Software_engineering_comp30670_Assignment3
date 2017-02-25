d1d47943020a2a44299b8bb21b79d31a5f9a8c46
Daniele Strafile <gerrystrafi@hotmail.it>
2017-02-25 21:58:16 +0000

    passed test_count, expected 400410 lights on and count is 400410!

--- src/led_tester.py
+++ src/led_tester.py
@@ -11,7 +11,8 @@ def read_uri(fname):
     if fname.startswith('http'):
         req = urllib.request.urlopen(fname)
         buffer = req.read().decode('utf-8')
-        return buffer
+        lines = buffer.splitlines()
+        return lines
     else:
         buffer = open(fname).read()
         return buffer
@@ -19,7 +20,6 @@ def read_uri(fname):
             
 uri = "http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3.txt"
 
-read_uri(uri)
 
 def extract(line):
     # check that line contains one of the three possible commands at the beginning
@@ -38,17 +38,61 @@ def extract(line):
                 
 # create square             
 def LED_TESTER(value):
-    list_false = [ [False]*value for _ in range(value) ]    
-    return list_false
+    if value > 0 and value < 10**9:
+        list_false = [ [False]*value for _ in range(value) ]    
+        return list_false
 
-# count number of values in square
-def size_tester(square):
+# count number of lights which are turned on in square
+def lights_number(square):
     count = 0
     for x in square:
         for j in x:
-            if j == False:
+            if j == True:
                 count += 1
-    return count
+    return count 
+    
+
+# this is the function to turn off/on/ switch lights
+def turn_switch(file):
+    read_file = read_uri(file)
+    for i, line in enumerate(read_file):
+        if i == 0:
+            n = int(line)
+            square = LED_TESTER(n)
+        else:
+            instructions = extract(line)
+            if instructions[0] == "turn on":
+                for i in range(instructions[1],instructions[3] + 1):
+                    for j in range(instructions[2], instructions[4] + 1):
+                        square[i][j] = True
+            elif instructions[0] == "turn off":
+                for i in range(instructions[1],instructions[3] + 1):
+                    for j in range(instructions[2], instructions[4] + 1):
+                        square[i][j] = False
+            elif instructions[0] == "switch":
+                for i in range(instructions[1],instructions[3] + 1):
+                    for j in range(instructions[2], instructions[4] + 1):
+                        if square[i][j] == True:
+                            square[i][j] = False
+                        else:
+                            square[i][j] = True
+    return lights_number(square)
+                         
+turn_switch(uri)
+
+"""
+    
+    for i in range(10):
+        for j in range(10):
+            print(LED_TESTER(10)[i][j], end="")
+            print()
+            
+"""           
+            
+
+    
+    
+
 
     
 
